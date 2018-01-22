aeb1a5a2a07459aaf0ef98edcd97dacb80c1e3c8
谢育初 <524945638@qq.com>
2018-01-22 18:29:30 +0800

    3.2.1 first test

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper' #注意是rails_helper
+
+describe "Static pages", :type => :feature do #注意是feature
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit "/static_pages/home"
+      expect(page).to have_content("Sample App")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('help')
+    end
+  end
+end
